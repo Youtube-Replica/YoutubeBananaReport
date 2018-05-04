3d90028444b4053d47f1b68c8012ebd5df1cd93b
Omar Abdelwahab <oagamingz@gmail.com>
2018-05-04 23:24:20 +0200

    - removed QoS

--- src/main/java/ReportService.java
+++ src/main/java/ReportService.java
@@ -30,8 +30,6 @@ public class ReportService {
 
             channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
 
-            channel.basicQos(1);
-
             System.out.println(" [x] Awaiting RPC requests");
 
             Consumer consumer = new DefaultConsumer(channel) {
